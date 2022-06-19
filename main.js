5f865066141779b4b2ec3144bef95d29f67f09dc
Vitor Orsini <vitorl85@gmail.com>
2022-06-18 21:29:17 -0300

    style.css: ajuste na largura maxima da página

--- main.js
+++ main.js
@@ -2,22 +2,22 @@ window.addEventListener("scroll", onScroll);
 
 onScroll();
 function onScroll() {
-    showNavOnScroll();
+  showNavOnScroll();
 }
 
 function showNavOnScroll() {
-    const menu = document.querySelector("#navigation");
-    if (scrollY > 0) {
-        menu.classList.add("scroll");
-    } else {
-        menu.classList.remove("scroll");
-    }
+  const menu = document.querySelector("#navigation");
+  if (scrollY > 0) {
+    menu.classList.add("scroll");
+  } else {
+    menu.classList.remove("scroll");
+  }
 }
 
 function openMenu() {
-    document.body.classList.add("menu-expanded");
+  document.body.classList.add("menu-expanded");
 }
 
 function closeMenu() {
-    document.body.classList.remove("menu-expanded");
+  document.body.classList.remove("menu-expanded");
 }
